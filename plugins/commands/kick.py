ba1761630d5a0131a1d2dd137c095a2c278cdab8
0x310 <104393390+Renovsk@users.noreply.github.com>
2025-04-28 13:31:02 -0400

    Update kick.py

--- plugins/commands/kick.py
+++ plugins/commands/kick.py
@@ -6,7 +6,7 @@ def kick(username, server):
         response = requests.post('http://localhost:6969/connect', json={
             "host": server, "port": 25565, "username": username
         })
-        if response.status_code != 200: return error(f'Failed to connect [{response.status_code}]')
+        if response.status_code != 200 and response.status_code != 400: return error(f'Failed to connect [{response.status_code}]')
         if not requests.get('http://localhost:6969/status').json().get('connected'):
             return error('Bot not connected.')
 
