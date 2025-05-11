b341268fc2c9a6599a7b5a931aceee725f60df8a
0x310 <104393390+Renovsk@users.noreply.github.com>
2025-05-10 22:38:31 -0400

    irrelevant push

--- scripts/example.py
+++ scripts/example.py
@@ -1,5 +1,5 @@
 arguments = ["name"]
-usage = "hello <name>\nSays hello to the specified name."
+usage = "example <name>\nSays hello to the specified name."
 
 def run(args):
     print(f"Hello {args['name']}!")
