19e4357a23ed70e1ee518d49c60d9ae9e8f7b91e
0x310 <104393390+Renovsk@users.noreply.github.com>
2025-05-19 16:18:32 -0400

    proxy check shit
    found a way to make it way faster and not false any proxies, idk how retarded it is but if it works don't touch it

--- plugins/commands/fetch.py
+++ plugins/commands/fetch.py
@@ -1,21 +1,38 @@
 import threading
-import requests
+import socks
 from plugins.common import *
+import urllib
 
 zeepa = []
 check = 0
 lock = threading.Lock()
 
-def checkproxy(proxy):
+nigga = {
+    'socks5': socks.SOCKS5,
+    'socks4': socks.SOCKS4,
+    'http':   socks.HTTP
+}
+
+def checkproxy(proxy: str):
     global check
-    proxies = {'http': proxy, 'https': proxy}
     try:
-        if requests.get('https://google.com', proxies=proxies, timeout=5).ok:
-            with lock: zeepa.append(proxy)
-    except: pass
+        if '://' not in proxy: proxy = 'http://' + proxy
+        p = urllib.parse.urlparse(proxy)
+        scheme = p.scheme.lower()
+        host = p.hostname
+        port = p.port
+        if scheme not in nigga or host is None or port is None: raise ValueError('Bad proxy string')
+        s = socks.socksocket()
+        s.set_proxy(nigga[scheme], host, port, username=p.username, password=p.password)
+        s.settimeout(5)
+        s.connect(('1.1.1.1', 80))
+        s.close()
+        with lock: zeepa.append(proxy)
+    except Exception: pass
     finally:
-        check += 1
-        print(f"\r{yellow}Checked ({white}{check}{yellow}) Good ({white}{len(zeepa)}{yellow})", end="", flush=True)
+        with lock:
+            check += 1
+            print(f"\rcheck ({check}) | Good ({len(zeepa)})", end='', flush=True)
 
 def fetch(ptype):
     with open('proxies.txt', 'w') as f: f.write('') # one ohio one ohio one BIG ohio
@@ -33,3 +50,4 @@ def fetch(ptype):
     with open('proxies.txt', 'a+') as f:
         for proxy in zeepa:
             f.write(f'{proxy}\n')
+    print('\n')
